61bc8cfa1e74c43009f83f6a6489f7f41ec919b2
Thierry Touin <thierrytouin@gmail.com>
2021-11-12 17:18:24 +0100

    switch to postgresql & add omnidb

--- start.sh
+++ start.sh
@@ -1,18 +1,17 @@
 
 #!/bin/bash
-
-echo " Starting MySQL server"
-sudo docker-compose up -d mysql
+echo " Starting Postgresql server"
+docker-compose up -d postgresql
 sleep 3s
 echo " Done."
 
 echo " Starting ElasticSearch server"
-sudo docker-compose up -d elasticsearch
+docker-compose up -d elasticsearch
 sleep 3s
 echo " Done."
 
 echo " Starting Kibana server"
-sudo docker-compose up -d kibana
+docker-compose up -d kibana
 sleep 3s
 echo " Done."
 
@@ -20,5 +19,5 @@ echo " Waiting for readiness of dependent servers ..."
 ./utils/spinner.sh sleep 5m
 
 echo " Starting Liferay server"
-sudo docker-compose up -d tomcat
-sudo docker-compose logs --follow tomcat
+docker-compose up -d tomcat
+docker-compose logs --follow tomcat
---
+++ utils/omnidb/omnidb-start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh 
+docker-compose -f ./omnidb-compose.yml up
+echo "OmniDB started at \e[92mhttp://localhost:8000 \e[0m"
